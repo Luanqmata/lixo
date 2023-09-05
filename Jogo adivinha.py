b09caf34261a89c3ecc1cd3a66c968831ea79190
Luanqmata <138558778+Luanqmata@users.noreply.github.com>
2023-09-05 04:14:18 -0300

    Update Jogo adivinha.py

--- Jogo adivinha.py
+++ Jogo adivinha.py
@@ -16,4 +16,29 @@ while True:
     else:
         print(f"você errou tente novamente.")
         numero_escolhido = int(input('digite outro numero: entre 0 e 10 '))
+________________________________________________________________________________________________________________________________________
+import random
+
+def jokenpo():
+    opcoes_do_jogo = ['pedra', 'papel', 'tesoura']
+    selecao_do_computador = random.choice(opcoes_do_jogo)
+    selecao_do_usuario = opcoes_do_jogo[int(input('Escolha entre pedra, papel ou tesoura: \n 1. pedra \n 2. papel \n 3. tesoura \n'))-1]
+    print(f'O computador escolheu {selecao_do_computador}.')
+    print(f'O usuário escolheu {selecao_do_usuario}.')
+
+    if selecao_do_usuario == selecao_do_computador:
+        print('Empate!')
+    elif selecao_do_usuario == 'pedra' and selecao_do_computador == 'tesoura':
+        print('Você ganhou!')
+    elif selecao_do_usuario == 'papel' and selecao_do_computador == 'pedra':
+        print('Você ganhou!')
+    elif selecao_do_usuario == 'tesoura' and selecao_do_computador == 'papel':
+        print('Você ganhou!')
+    else:
+        print('Você perdeu!')
+    input('Pressione ENTER para jogar novamente...')
+    jokenpo()
+
+jokenpo()
+
 
