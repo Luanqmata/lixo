7ffa1877aebea060403f4b1bedaf9e8f677c2254
Luanqmata <138558778+Luanqmata@users.noreply.github.com>
2023-09-05 21:43:36 -0300

    adivinha.py

--- Jogo adivinha.py
+++ Jogo adivinha.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 def jogo_adivinha():
     numero_aleatorio = random.randint(0,10)
     return numero_aleatorio
@@ -7,15 +7,16 @@ def jogo_adivinha():
 numero_do_jogo = jogo_adivinha()
 tentativas = 0
 
+
 while True:
     numero_escolhido = int(input("Digite o numero do jogo adivinha: '0 a 10'. "))
-    tentativas += 1 
-    if(numero_do_jogo == numero_escolhido ):
+    tentativas += 1
+    if numero_do_jogo == numero_escolhido :
         print(f"parabens você acertou. e teve {tentativas} tentativas!")
         break
     else:
         print(f"você errou tente novamente.")
-        numero_escolhido = int(input('digite outro numero: entre 0 e 10 '))
+        time.sleep(0.7)
 ________________________________________________________________________________________________________________________________________
 import random
 
