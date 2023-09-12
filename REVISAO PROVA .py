ed5e613c96267056b79283fab57c53aed9c6b979
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-11 23:41:25 -0300

    Update REVISAO PROVA .py

--- REVISAO PROVA .py
+++ REVISAO PROVA .py
@@ -61,6 +61,15 @@ def area_circulo():
 
 print(area_circulo())
 ## 6
+def calcular_signo():
+    ano_nascimento = int(input("Digite o ano de nascimento: "))
+    signos_chineses = ["Rato", "Boi", "Tigre", "Coelho", "Dragão", "Serpente", "Cavalo", "Cabra", "Macaco", "Galo", "Cachorro", "Porco"]
+    ano_base = 1900  
+    indice_signo = (ano_nascimento - ano_base) % 12
+    return signos_chineses[indice_signo]
+
+signo = calcular_signo()
+print(f"Seu signo chinês é {signo}")
 ## 7
 def numero_primo():
     numero = int(input('Digite um numero: '))
