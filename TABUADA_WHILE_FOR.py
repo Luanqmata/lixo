95582851751cc1e74f45214c046b95f670e7de49
*Luan Calazans* <138558778+Luanqmata@users.noreply.github.com>
2023-09-20 04:34:10 -0300

    teoria

--- TABUADA_WHILE_FOR.py
+++ TABUADA_WHILE_FOR.py
@@ -1,4 +1,5 @@
-                                                  
+# FOR FOR FOR FOR FOR FOR FOR FOR FOR FOR FOR FOR FOR FOR FOR FOR        
+
                                                             ## CONTAGEM DO '1 AO 10'
 def contagem():
     começo = int(input('Digite o começo da contagem.'))
@@ -37,3 +38,53 @@ def tabuada_1_10():
         
         
 tabuada_1_10()
+
+##____________________________________________________________________________________________________________
+
+## WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE WHILE 
+
+##                                                                         CONTADOR DO 1 AO 10 
+
+## DO 1 AO 10 
+incio = 1
+final = 10
+while incio <= final:
+    print(incio)
+    incio +=1
+## DO 10 AO 1 
+inicio = 10
+while inicio >=1:
+    print(inicio)
+    inicio -=1
+
+##__________________________________________________________________________________________________________________
+
+import time
+def tabuada_while(numero_da_tabuada):
+    print('\n')
+    i = 1
+    while i <= 10:
+        conta = i * numero_da_tabuada
+        print(f'  {i} x {user_tabuada} = {conta}')
+        time.sleep(0.5)
+        i +=1
+        
+user_tabuada = int(input(' digite o numero para realizar a tabuada: '))
+tabuada_while(user_tabuada)
+
+##_____________________________________________________________________________________________________________________
+
+def tabuada_1_10_while():
+    print('\n@@@@@@@@@@@@ Tabuada do 1 ao 10 WHILE @@@@@@@@@@@@\n')
+    a = 1
+    while a <= 10:
+        b = 1 # Reinicializa o contador b a cada iteração de a
+        print(f'\nTABUADA DO {a}\n')
+        while b <=10:
+            conta =a*b
+            print(f'{a} x {b} = {conta}')
+            b+=1
+        a +=1
+            
+tabuada_1_10_while()
+
